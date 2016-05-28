34574586387e511154d4b883f5fe7d0b69500591
Jason Romero <codenamejason@outlook.com>
2016-05-28 10:59:20 -0400

    Add project files.

---
+++ KendoUIDrawer1/app.js
@@ -0,0 +1,58 @@
+'use strict';
+
+(function() {
+    var app = {
+        data: {}
+    };
+
+    var bootstrap = function() {
+        $(function() {
+            app.mobileApp = new kendo.mobile.Application(document.body, {
+                skin: 'nova',
+                initial: 'components/homeView/view.html'
+            });
+        });
+    };
+
+    if (window.cordova) {
+        document.addEventListener('deviceready', function() {
+            if (navigator && navigator.splashscreen) {
+                navigator.splashscreen.hide();
+            }
+
+            var element = document.getElementById('appDrawer');
+            if (typeof(element) != 'undefined' && element !== null) {
+                if (window.navigator.msPointerEnabled) {
+                    $('#navigation-container').on('MSPointerDown', 'a', function(event) {
+                        app.keepActiveState($(this));
+                    });
+                } else {
+                    $('#navigation-container').on('touchstart', 'a', function(event) {
+                        app.keepActiveState($(this));
+                    });
+                }
+            }
+
+            bootstrap();
+        }, false);
+    } else {
+        bootstrap();
+    }
+
+    app.keepActiveState = function _keepActiveState(item) {
+        var currentItem = item;
+        $('#navigation-container li.active').removeClass('active');
+        currentItem.addClass('active');
+    };
+
+    window.app = app;
+
+    app.isOnline = function() {
+        if (!navigator || !navigator.connection) {
+            return true;
+        } else {
+            return navigator.connection.type !== 'none';
+        }
+    };
+}());
+
