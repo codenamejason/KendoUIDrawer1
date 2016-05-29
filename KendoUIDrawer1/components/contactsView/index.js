1ad0dde638d3303b050c49d6931a0bcb3fee7dde
Jason Romero <codenamejason@outlook.com>
2016-05-29 01:11:13 -0400

    add folders and files, still not a build ready

--- KendoUIDrawer1/components/contactsView/index.js
+++ KendoUIDrawer1/components/contactsView/index.js
@@ -8,10 +8,10 @@ app.contactsView = kendo.observable({
 // START_CUSTOM_CODE_contactsView
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 (function () {
-    app.contactsView.set('title', 'Contacts');
+    app.contactsView.set('title', 'Shoes');
     
     var dataSource = new kendo.data.DataSource({
-        data: [{ id: 1, name: 'Bob' }, { id: 2, name: 'Mary' }, { id: 3, name: 'John' }]
+        data: [{ id: 1, name: 'Heel' }, { id: 2, name: 'Flat' }, { id: 3, name: 'Sandal' }]
     });
     app.contactsView.set('dataSource', dataSource);
     
--- KendoUIDrawer1/components/productsView/index.js
+++ KendoUIDrawer1/components/productsView/index.js
@@ -1,13 +1,13 @@
 'use strict';
 
-app.homeView = kendo.observable({
-    onShow: function() {},
-    afterShow: function() {}
+app.productView = kendo.observable({
+    onShow: function () { },
+    afterShow: function () { }
 });
 
 // START_CUSTOM_CODE_homeView
 // Add custom code here. For more information about custom code, see http://docs.telerik.com/platform/screenbuilder/troubleshooting/how-to-keep-custom-code-changes
 (function () {
-    app.homeView.set('title', 'Nice Shoes');
+    app.productView.set('title', 'Documanager 1.0');
 })();
 // END_CUSTOM_CODE_homeView
